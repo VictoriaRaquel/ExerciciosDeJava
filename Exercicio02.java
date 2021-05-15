3adb8452601b8709222152e28d0e084f04412682
Victoria Raquel Ferreira <v.raquelferreira@bol.com.br>
2021-05-15 19:20:46 -0300

    Create Exercicio02.java

---
+++ Exercicio02.java
@@ -0,0 +1,10 @@
+//Mostre ao usuário a Frase "1º Primeiro teste do Java !!!!".
+package exerciciofrase002;
+
+public class ExercicioFrase002 {
+
+    public static void main(String[] args) {
+       System.out.println(" 1º Primeiro teste do java !!!");
+    }
+    
+}
