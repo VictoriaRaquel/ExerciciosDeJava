15d787295faa7801f1e4e1d9dd6783a5b84e9766
Victoria Raquel Ferreira <v.raquelferreira@bol.com.br>
2021-05-15 19:18:06 -0300

    Create Exercicio01.java

---
+++ Exercicio01.java
@@ -0,0 +1,10 @@
+/*Mostre ao usuário a Palavra "Teste";, o nome da Classe será ExercicioTeste, deve ter construtor.*/
+package exercicioteste001;
+
+public class ExercicioTeste001 {
+
+    public static void main(String[] args) {
+       System.out.println("Teste");
+    }
+    
+}
